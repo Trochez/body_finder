6f9a0c77c37fa1249079c5a118cea191c0f69b09
Trochez <tromon92@gmail.com>
2026-08-13 18:06:52 -0500

    test: settle capability scan before launch assertions

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@ import 'package:body_finder/main.dart';
 void main() {
   testWidgets('shows scientific safety status on launch', (tester) async {
     await tester.pumpWidget(const BodyFinderApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('Body Finder'), findsOneWidget);
     expect(find.text('Experimental anomaly mapping'), findsOneWidget);
