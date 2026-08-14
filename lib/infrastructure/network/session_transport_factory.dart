d7b5ffa594a8301c4178bb66849a70b12975d45f
Trochez <tromon92@gmail.com>
2026-08-14 17:11:55 -0500

    feat: use address-first BLE session transport on Linux

--- lib/infrastructure/network/session_transport_factory.dart
+++ lib/infrastructure/network/session_transport_factory.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'android_ble_session_platform_adapter.dart';
 import 'ble_session_transport.dart';
-import 'linux_bluez_session_platform_adapter.dart';
+import 'linux_address_first_ble_session_platform_adapter.dart';
 import 'relaying_session_transport.dart';
 import 'session_transport.dart';
 import 'udp_lan_transport.dart';
@@ -26,7 +26,7 @@ SessionTransport createDefaultSessionTransport({
     children.add(
       BleSessionTransport(
         nodeId: nodeId,
-        platformAdapter: LinuxBluezSessionPlatformAdapter(),
+        platformAdapter: LinuxAddressFirstBleSessionPlatformAdapter(),
       ),
     );
   }
